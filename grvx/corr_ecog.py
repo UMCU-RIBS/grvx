ea90a6540365a01e12e79a0a15af054a347e523c
Gio at UMCU <github@gpiantoni.com>
2018-01-15 12:55:16 +0100

    run all corr

--- grvx/corr_ecog.py
+++ grvx/corr_ecog.py
@@ -1,4 +1,5 @@
 from boavus.ieeg.corr_fmri import _main
+from numpy import arange
 from .core.constants import (PARAMETERS,
                              FEAT_PATH,
                              FREESURFER_PATH,
@@ -15,5 +16,6 @@ def Corr_ECoG_fMRI(lg, img_dir):
     for ieeg_file in DATA_PATH.rglob('*_ieeg.bin'):
         feat_path = FEAT_PATH / 'sub-ommen/ses-daym25/func/sub-ommen_ses-daym25_task-motor-hand-left_run-00.feat'
 
-        result = _main(ieeg_file, feat_path, FREESURFER_PATH, DERIVATIVES_PATH, [5, ])
+        result = _main(ieeg_file, feat_path, FREESURFER_PATH, DERIVATIVES_PATH,
+                arange(1, 10, .5))
         print(result)
