315f9a3094c2cbb86fd12f3a9b275525a3deb821
Gio at UMCU <github@gpiantoni.com>
2018-01-16 14:26:11 +0100

    nice graph for ommen and bunnik

--- grvx/corr_ecog.py
+++ grvx/corr_ecog.py
@@ -1,4 +1,5 @@
 from boavus.ieeg.corr_fmri import _main, _main_to_elec
+from bidso import iEEG
 from numpy import arange
 from .core.constants import (PARAMETERS,
                              FEAT_PATH,
@@ -24,8 +25,13 @@ def Corr_ECoG_fMRI(lg, img_dir):
 @with_log
 def Corr_fMRI_to_Elec(lg, img_dir):
     for ieeg_file in DATA_PATH.rglob('*_ieeg.bin'):
-        feat_path = FEAT_PATH / 'sub-ommen/ses-daym25/func/sub-ommen_ses-daym25_task-motor-hand-left_run-00.feat'
+        ieeg = iEEG(ieeg_file)
+        feat_path = next((FEAT_PATH / f'sub-{ieeg.subject}').rglob('*.feat'))
 
         result = _main_to_elec(ieeg_file, feat_path, FREESURFER_PATH, DERIVATIVES_PATH,
-                               arange(1, 10))
+                               arange(1, 10), True)
         print(result)
+
+        result_val = img_dir / f'corr_{ieeg_file.stem}.csv'
+        with result_val.open('w') as f:
+            f.write(','.join(str(x) for x in result))
--- grvx/read_bids.py
+++ grvx/read_bids.py
@@ -7,15 +7,19 @@ from xelo2bids.bids_tree import create_bids, DF_TASKS
 from .core.constants import DATA_PATH
 from .core.log import with_log
 
+{'subj': 'duiven', 'ECoG': 'duiven_20170212-102941-001_TaskXML', 'fMRI': '516bec27a411e89fe71fbaee7783c014'},
+{'subj': 'ommen', 'ECoG': 'ommen_20170114-110422-013_TaskXML', 'fMRI': 'ffa534bac2f46a876d04dd5129bb8f5a'},
+
 df = [
-    {'subj': 'duiven', 'ECoG': 'duiven_20170212-102941-001_TaskXML', 'fMRI': '516bec27a411e89fe71fbaee7783c014'},
-    {'subj': 'ommen', 'ECoG': 'ommen_20170114-110422-013_TaskXML', 'fMRI': 'ffa534bac2f46a876d04dd5129bb8f5a'},
+    {'subj': 'vledder', 'ECoG': 'vledder_20170116-100726-001_TaskXML', 'fMRI': 'bc2b8247f88b8027599f0bf806dc389c'},
+    {'subj': 'bunnik', 'ECoG': 'bunnik_bunnik011116_motor_right_hand_streched_palmUpS001R01_TaskXML', 'fMRI': 'bunnik_bunnik160916_WIP_Circle_MotorHand_PrestoSense40sl_u_SENSE_8_1_TaskXML'},
     ]
 commontasks = DataFrame(df)
 
 
 @with_log
 def Read_As_Bids(lg, img_dir):
+    """
     try:
         rmtree(DATA_PATH)
     except:
@@ -25,6 +29,7 @@ def Read_As_Bids(lg, img_dir):
         DATA_PATH.mkdir()
     except:
         pass
+    """
 
     ALL_SUBJ = set(commontasks.subj)
     stems = DF_TASKS.loc[(DF_TASKS['SubjectCode'].isin(ALL_SUBJ)) & (DF_TASKS['TaskName'] == 't1_anatomy_scan'), 'stem']
