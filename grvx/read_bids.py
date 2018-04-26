d8e9b033dacad7b247355a0da8fe3ea62aeaad5d
Gio at UMCU <github@gpiantoni.com>
2018-04-26 15:35:43 +0200

    skip defacing and add 3 more tasks

--- grvx/read_bids.py
+++ grvx/read_bids.py
@@ -3,11 +3,14 @@ from shutil import rmtree
 from pandas import DataFrame
 
 from xelo2bids.bids_tree import create_bids, DF_TASKS
+from xelo2bids import bids_mri
 
 from .core.constants import DATA_PATH
 from .core.log import with_log
 
+bids_mri.RUN_DEFACE = False
 
+# only TR = 0.6
 df = [
     {'subj': 'arnhem', 'ECoG': 'arnhem2097', 'fMRI': 'd44f92264b48326c0c365b86c6e9b6e1'},
     {'subj': 'boxtel', 'ECoG': 'boxtel1964', 'fMRI': '5dcbae36c46e31039d2cdd4155a85f2e'},
@@ -16,9 +19,12 @@ df = [
     {'subj': 'duiven', 'ECoG': 'duiven_20170212-102941-001_TaskXML', 'fMRI': '516bec27a411e89fe71fbaee7783c014'},
     {'subj': 'erp', 'ECoG': 'erp1827', 'fMRI': 'erp1930'},
     {'subj': 'gord', 'ECoG': 'gord536', 'fMRI': 'gord518'},
+    {'subj': 'groo', 'ECoG': 'groo47', 'fMRI': 'groo45'},
+    {'subj': 'heek', 'ECoG': 'heek_20180217-143702-003_TaskXML', 'fMRI': 'heek_V7711_fMRI_MotorHand-Circle_14_1_TaskXML'},
     {'subj': 'itens', 'ECoG': 'itens_Itens_08032017_Motor_L_fingertappingS001R02_TaskXML', 'fMRI': '814a028cd01fbdb3949626f6b2e85018'},
     {'subj': 'maas', 'ECoG': 'maas480', 'fMRI': 'maas470'},
     {'subj': 'ommen', 'ECoG': 'ommen_20170114-110422-013_TaskXML', 'fMRI': 'ffa534bac2f46a876d04dd5129bb8f5a'},
+    {'subj': 'ruit', 'ECoG': 'ruit590', 'fMRI': 'ruit570'},
     {'subj': 'smilde', 'ECoG': 'smilde1969', 'fMRI': '98f3b01b064e8a78a942cbb7545bab91'},
     {'subj': 'spoo', 'ECoG': 'spoo371', 'fMRI': 'spoo360'},
     {'subj': 'tesf', 'ECoG': 'tesf454', 'fMRI': 'tesf446'},
