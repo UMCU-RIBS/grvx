3bdbb78aa2e218cb0907601fbada2067101443e9
Gio at UMCU <github@gpiantoni.com>
2018-05-17 15:40:31 +0200

    fix electrodes

--- grvx/electrodes.py
+++ grvx/electrodes.py
@@ -1,10 +1,10 @@
-from boavus import boavus
-
+from boavus.ieeg import (project_electrodes,
+                         assign_regions,
+                         )
 from .core.constants import (FREESURFER_PATH,
                              DATA_PATH,
                              ANALYSIS_PATH,
                              OUTPUT_PATH,
-                             Parameters_Json,
                              )
 from .core.log import with_log
 
@@ -12,71 +12,19 @@ from .core.log import with_log
 @with_log
 def Project_Elec_On_Surf(lg, img_dir):
 
-    PARAMETERS_JSON = Parameters_Json({'acquisition': '*al', 'parallel': True})
-    boavus([
-        'ieeg',
-        'project_electrodes',
-        '--bids_dir',
-        str(DATA_PATH),
-        '--freesurfer_dir',
-        str(FREESURFER_PATH),
-        '--analysis_dir',
-        str(ANALYSIS_PATH),
-        '--log',
-        'debug',
-        '--parameters',
-        PARAMETERS_JSON.name,
-        ])
-    PARAMETERS_JSON.delete()
-
-    PARAMETERS_JSON = Parameters_Json({'acquisition': '*projected'})
-    boavus([
-        'ieeg',
-        'plot_electrodes',
-        '--bids_dir',
-        str(DATA_PATH),
-        '--freesurfer_dir',
-        str(FREESURFER_PATH),
-        '--analysis_dir',
-        str(ANALYSIS_PATH),
-        '--output_dir',
-        str(OUTPUT_PATH),
-        '--log',
-        'debug',
-        '--parameters',
-        PARAMETERS_JSON.name,
-        ])
-    PARAMETERS_JSON.delete()
+    project_electrodes.main(
+        bids_dir=DATA_PATH,
+        freesurfer_dir=FREESURFER_PATH,
+        analysis_dir=ANALYSIS_PATH,
+        acquisition='ctmr',
+        )
 
 
 @with_log
 def Assign_Regions_To_Elec(lg, img_dir):
-    PARAMETERS_JSON = Parameters_Json({'acquisition': '*projected', 'parallel': True})
-    boavus([
-        'ieeg',
-        'assign_regions',
-        '--bids_dir',
-        str(DATA_PATH),
-        '--freesurfer_dir',
-        str(FREESURFER_PATH),
-        '--log',
-        'debug',
-        '--parameters',
-        PARAMETERS_JSON.name,
-        ])
-    PARAMETERS_JSON.delete()
 
-    PARAMETERS_JSON = Parameters_Json({'acquisition': '*ctmr', 'parallel': True})
-    boavus([
-        'ieeg',
-        'assign_regions',
-        '--bids_dir',
-        str(DATA_PATH),
-        '--freesurfer_dir',
-        str(FREESURFER_PATH),
-        '--log',
-        'debug',
-        '--parameters',
-        PARAMETERS_JSON.name,
-        ])
-    PARAMETERS_JSON.delete()
+    assign_regions.main(
+        bids_dir=DATA_PATH,
+        freesurfer_dir=FREESURFER_PATH,
+        acquisition='*projected',
+        )
