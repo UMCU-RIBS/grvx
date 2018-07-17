22d85337552c26354be25b3d2ec0e400e7239eb6
Gio at UMCU <github@gpiantoni.com>
2018-07-17 11:38:58 +0200

    add analysis_dir

--- grvx/nipype/workflow.py
+++ grvx/nipype/workflow.py
@@ -5,6 +5,7 @@ from boavus.freesurfer.reconall import node_reconall
 from boavus.nipype import (function_ieeg_read,
                            function_ieeg_preprocess,
                            function_ieeg_frequency,
+                           function_ieeg_compare,
                            FEAT_model,
                            )
 
@@ -30,20 +31,24 @@ def create_grvx_workflow():
     node_ieeg_frequency.inputs.taper = ''
     node_ieeg_frequency.inputs.duration = 2
 
+    node_ieeg_compare = Node(function_ieeg_compare, name='ieeg_compare')
+    node_ieeg_compare.inputs.analysis_dir = str(ANALYSIS_PATH)
+
     w = Workflow('grvx')
     w.base_dir = str(NIPYPE_PATH)
     # w.connect(bids, 'subject', node_reconall, 'subject_id')
     # w.connect(bids, 'anat', node_reconall, 'T1_files')
 
-    w.connect(bids, 'func', node_featdesign, 'func')
-    w.connect(bids, 'anat', node_featdesign, 'anat')
+    # w.connect(bids, 'func', node_featdesign, 'func')
+    # w.connect(bids, 'anat', node_featdesign, 'anat')
 
-    w.connect(node_featdesign, 'fsf_file', node_feat, 'fsf_file')
+    # w.connect(node_featdesign, 'fsf_file', node_feat, 'fsf_file')
 
     w.connect(bids, 'ieeg', node_ieeg_read, 'ieeg')
     w.connect(bids, 'elec', node_ieeg_read, 'electrodes')
     w.connect(node_ieeg_read, 'ieeg_files', node_ieeg_preprocess, 'ieeg')
     w.connect(node_ieeg_preprocess, 'ieeg', node_ieeg_frequency, 'ieeg')
+    w.connect(node_ieeg_frequency, 'ieeg', node_ieeg_compare, 'in_files')
 
     w.write_graph(graph2use='flat')
 
