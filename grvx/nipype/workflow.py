70731f0dc008c9a6aad39171a6518d3720347019
Gio at UMCU <github@gpiantoni.com>
2018-07-17 12:44:28 +0200

    add all the nodes

--- grvx/nipype/workflow.py
+++ grvx/nipype/workflow.py
@@ -7,10 +7,13 @@ from boavus.nipype import (function_ieeg_read,
                            function_ieeg_frequency,
                            function_ieeg_compare,
                            FEAT_model,
+                           function_fmri_compare,
+                           function_fmri_atelec,
+                           function_corr,
                            )
 
 from .bids import bids, SUBJECTS
-from ..core.constants import NIPYPE_PATH, FREESURFER_PATH, ANALYSIS_PATH
+from ..core.constants import NIPYPE_PATH, FREESURFER_PATH, ANALYSIS_PATH, OUTPUT_PATH
 
 
 def create_grvx_workflow():
@@ -21,6 +24,19 @@ def create_grvx_workflow():
     node_featdesign.inputs.analysis_dir = str(ANALYSIS_PATH)
 
     node_feat = Node(FEAT(), name='feat')
+
+    node_fmri_compare = Node(function_fmri_compare, name='fmri_compare')
+    node_fmri_compare.inputs.analysis_dir = str(ANALYSIS_PATH)
+    node_fmri_compare.inputs.measure = 'percent'
+    node_fmri_compare.inputs.normalize_to_mean = False
+
+    node_fmri_atelec = Node(function_fmri_atelec, name='fmri_atelec')
+    node_fmri_atelec.inputs.upsample = False
+    node_fmri_atelec.inputs.graymatter = False
+    node_fmri_atelec.inputs.distance = 'gaussian'
+    node_fmri_atelec.inputs.kernel_start = 4
+    node_fmri_atelec.inputs.kernel_end = 9
+    node_fmri_atelec.inputs.kernel_step = 1
 
     node_ieeg_read = Node(function_ieeg_read, name='ieeg_read')
     node_ieeg_read.inputs.analysis_dir = str(ANALYSIS_PATH)
@@ -34,21 +50,31 @@ def create_grvx_workflow():
     node_ieeg_compare = Node(function_ieeg_compare, name='ieeg_compare')
     node_ieeg_compare.inputs.analysis_dir = str(ANALYSIS_PATH)
 
+    node_corr = Node(function_corr, name='corr_fmri_ecog')
+    node_corr.inputs.output_dir = str(OUTPUT_PATH)
+    node_corr.inputs.PVALUE = 0.05
+
     w = Workflow('grvx')
     w.base_dir = str(NIPYPE_PATH)
     # w.connect(bids, 'subject', node_reconall, 'subject_id')
     # w.connect(bids, 'anat', node_reconall, 'T1_files')
 
-    # w.connect(bids, 'func', node_featdesign, 'func')
-    # w.connect(bids, 'anat', node_featdesign, 'anat')
+    w.connect(bids, 'func', node_featdesign, 'func')
+    w.connect(bids, 'anat', node_featdesign, 'anat')
 
-    # w.connect(node_featdesign, 'fsf_file', node_feat, 'fsf_file')
+    w.connect(node_featdesign, 'fsf_file', node_feat, 'fsf_file')
+    w.connect(node_feat, 'feat_dir', node_fmri_compare, 'feat_path')
+
+    w.connect(node_fmri_compare, 'out_file', node_fmri_atelec, 'measure_nii')
 
     w.connect(bids, 'ieeg', node_ieeg_read, 'ieeg')
     w.connect(bids, 'elec', node_ieeg_read, 'electrodes')
     w.connect(node_ieeg_read, 'ieeg_files', node_ieeg_preprocess, 'ieeg')
     w.connect(node_ieeg_preprocess, 'ieeg', node_ieeg_frequency, 'ieeg')
     w.connect(node_ieeg_frequency, 'ieeg', node_ieeg_compare, 'in_files')
+
+    w.connect(node_ieeg_compare, 'tsv_compare', node_corr, 'ecog_file')
+    w.connect(node_fmri_atelec, 'out_file', node_corr, 'fmri_file')
 
     w.write_graph(graph2use='flat')
 
