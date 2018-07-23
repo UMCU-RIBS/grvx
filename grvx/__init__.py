ac53b3e12e3de64b3119ad3123bf5e803a73a648
Gio at UMCU <github@gpiantoni.com>
2018-07-23 11:15:36 +0200

    remove all the BOAVUS calls, now use nipype

--- grvx/__init__.py
+++ grvx/__init__.py
@@ -1,5 +1,1 @@
 from .read_bids import Read_As_Bids
-from .analysis_fmri import Run_fMRI_feat, Compare_Feat
-from .analysis_ecog import Compare_ECoG
-from .electrodes import Project_Elec_On_Surf, Assign_Regions_To_Elec
-from .corr_ecog import Corr_fMRI_to_Elec
