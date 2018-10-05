b79176f04a9d51e27527c7bea786aa44f5bc6f9b
Gio at UMCU <github@gpiantoni.com>
2018-10-05 16:29:13 +0200

    plot scatter

---
+++ grvx/plot/scatter.py
@@ -0,0 +1,76 @@
+from bidso.utils import read_tsv
+from boavus.corr.corrfmri import select_channels
+import plotly.graph_objs as go
+from pathlib import Path
+from exportimages import export_plotly
+
+pvalue = 0.05  # TODO: PARAMETERS
+
+
+def plot_scatter(wd):
+    summary = read_tsv(Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/summary_per_subject.tsv'))
+    ecog_file = Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/ecog/sub-delft_ses-UMCUECOGday01_task-motorHandLeft_run-1_acq-clinical_compare.tsv')
+    fmri_file = Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/fmri/sub-delft_ses-UMCU3Tdaym13_task-motorHandLeft_run-1_bold_compare.tsv')
+
+    fmri_tsv = read_tsv(fmri_file)
+    ecog_tsv = read_tsv(ecog_file)
+    fmri_tsv = select_channels(fmri_tsv, ecog_tsv)
+
+    kernel = str(summary[summary['subject'] == 'delft']['size_at_peak'].item())
+    slope = summary[summary['subject'] == 'delft']['slope_at_peak']
+    intercept = summary[summary['subject'] == 'delft']['intercept_at_peak']
+
+    x_ecog = ecog_tsv['measure']
+    y_fmri = fmri_tsv[kernel]
+
+    traces = [
+        go.Scatter(
+            name='not significant',
+            x=x_ecog[ecog_tsv['pvalue'] > pvalue],
+            y=y_fmri[ecog_tsv['pvalue'] > pvalue],
+            mode='markers',
+            marker=go.Marker(
+                symbol='circle-open',
+                color='black',
+                )
+            ),
+        go.Scatter(
+            name='significant',
+            x=x_ecog[ecog_tsv['pvalue'] <= pvalue],
+            y=y_fmri[ecog_tsv['pvalue'] <= pvalue],
+            mode='markers',
+            marker=go.Marker(
+                symbol='circle',
+                color='black',
+                )
+            ),
+        go.Scatter(
+            x=x_ecog,
+            y=slope * x_ecog + intercept,
+            mode='lines',
+            marker=go.Marker(
+                color='black'
+                ),
+            ),
+        ]
+
+    layout = go.Layout(
+        showlegend=False,
+        xaxis=dict(
+            tickfont=dict(
+                size=8,
+                ),
+            ),
+        yaxis=dict(
+            tickfont=dict(
+                size=8,
+                ),
+            ),
+        )
+
+    fig = go.Figure(
+        data=traces,
+        layout=layout,
+        )
+
+    export_plotly(fig, 'scatter.svg', int(3 * 96), int(5 * 96), wd)
--- grvx/plotting.py
+++ grvx/plotting.py
@@ -3,8 +3,10 @@ from exportimages import Webdriver
 from .core.log import with_log
 from .core.constants import PLOT_PATH
 
+from .plot.histogram import plot_histogram
 from .plot.gaussian import plot_gaussian
 from .plot.fmri import plot_fmri
+from .plot.scatter import plot_scatter
 
 
 @with_log
@@ -14,4 +16,5 @@ def Plot_Results(lg):
 
     with Webdriver(PLOT_PATH) as wd:
         plot_gaussian(wd)
-
+        plot_histogram(wd)
+        plot_scatter(wd)
