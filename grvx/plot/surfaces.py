f54e1cba33991ef39ade4c188e2c4e0226764ac8
Gio at UMCU <github@gpiantoni.com>
2018-10-09 12:26:41 +0200

    find electrodes

--- grvx/plot/surfaces.py
+++ grvx/plot/surfaces.py
@@ -1,35 +1,42 @@
 from wonambi.attr import Surf, Channels, Freesurfer
 from wonambi.viz import Viz3
-from numpy import hstack, vstack, array
+from numpy import hstack, vstack, array, median
+from bidso import Electrodes
+from bidso import file_Core
 from bidso.utils import read_tsv
+from bidso.find import find_in_bids
 
 from pathlib import Path
 
 results_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/ecog')
+plots_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/plots/surfaces')
 
-results_path = next(results_dir.glob('*.tsv'))
-results = read_tsv(results_path)
+def plot_surfaces():
+    for results_path in results_dir.glob('*.tsv'):
+        results = read_tsv(results_path)
 
-from bidso import file_Core
+        r = file_Core(results_path)
 
+        fs = Freesurfer(f'/Fridge/users/giovanni/projects/grvx/derivatives/freesurfer/sub-{r.subject}')
+        brain = fs.read_brain()
 
-r = file_Core(results_path)
+        electrodes_file = find_in_bids(Path('/Fridge/users/giovanni/projects/grvx/subjects/'), subject=r.subject, acquisition='*alctmrregions', modality='electrodes')
+        electrodes = Electrodes(electrodes_file)
 
-fs = Freesurfer(f'/Fridge/users/giovanni/projects/grvx/derivatives/freesurfer/sub-{r.subject}')
-brain = fs.read_brain()
+        labels = electrodes.electrodes.get(map_lambda=lambda x: x['name'])
+        chan_xyz = array(electrodes.get_xyz())
+        elec = Channels(labels, chan_xyz - fs.surface_ras_shift)
 
+        colors = array([0, 0, 0]) * (results['pvalue'] <= 0.05).astype(float)[:, None]
+        colors += array([0.8, 0.8, 0.8]) * (results['pvalue'] > 0.05).astype(float)[:, None]
 
-electrodes = Electrodes('/Fridge/users/giovanni/projects/grvx/subjects/sub-bunnik/ses-UMCUECOGday04/ieeg/sub-bunnik_ses-UMCUECOGday04_acq-clinicalctmrregions_electrodes.tsv')
-
-
-labels = electrodes.electrodes.get(map_lambda=lambda x: x['name'])
-chan_xyz = array(electrodes.get_xyz())
-elec = Channels(labels, chan_xyz - fs.surface_ras_shift)
-
-colors = array([0, 0, 0])  * (results['pvalue'] <= 0.05).astype(float)[:, None]
-colors += array([0.8, 0.8, 0.8])  * (results['pvalue'] > 0.05).astype(float)[:, None]
-
-v = Viz3()
-# v.add_chan(elec(lambda x: x.label in results['channel']), color=colors)
-v.add_surf(brain.lh)
-v.add_surf(brain.rh)
+        v = Viz3()
+        v.add_chan(elec(lambda x: x.label in results['channel']), color=colors)
+        if median(chan_xyz[:, 0]) > 0:
+            v.add_surf(brain.lh)
+            v.add_surf(brain.rh)
+        else:
+            v.add_surf(brain.rh)
+            v.add_surf(brain.lh)
+        v.save(plots_dir / (r.subject + '.png'))
+        v.close()
