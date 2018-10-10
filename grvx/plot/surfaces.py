8c5c1f525db549d4683e39b40f71efc227dfb8b3
Gio at UMCU <github@gpiantoni.com>
2018-10-10 10:01:30 +0200

    use pvalue

--- grvx/plot/surfaces.py
+++ grvx/plot/surfaces.py
@@ -11,6 +11,9 @@ from pathlib import Path
 results_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/ecog')
 plots_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/plots/surfaces')
 
+pvalue = 0.05  # TODO: PARAMETERS
+
+
 def plot_surfaces():
     for results_path in results_dir.glob('*.tsv'):
         results = read_tsv(results_path)
@@ -27,16 +30,20 @@ def plot_surfaces():
         chan_xyz = array(electrodes.get_xyz())
         elec = Channels(labels, chan_xyz - fs.surface_ras_shift)
 
-        colors = array([0, 0, 0]) * (results['pvalue'] <= 0.05).astype(float)[:, None]
-        colors += array([0.8, 0.8, 0.8]) * (results['pvalue'] > 0.05).astype(float)[:, None]
+        colors = array([0, 0, 0]) * (results['pvalue'] <= pvalue).astype(float)[:, None]
+        colors += array([0.8, 0.8, 0.8]) * (results['pvalue'] > pvalue).astype(float)[:, None]
 
         v = Viz3()
         v.add_chan(elec(lambda x: x.label in results['channel']), color=colors)
         if median(chan_xyz[:, 0]) > 0:
             v.add_surf(brain.lh)
             v.add_surf(brain.rh)
+            v._view.camera.azimuth = 120
         else:
             v.add_surf(brain.rh)
             v.add_surf(brain.lh)
+            v._view.camera.azimuth = -120
+
+        v._view.camera.elevation = 20
         v.save(plots_dir / (r.subject + '.png'))
         v.close()
